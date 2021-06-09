234f151e3e89e1edc302b03aa85e4f88b59b1147
justin <justinrvillena@gmail.com>
2021-06-08 17:18:06 -0700

    finished test

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const db = new Database({
     password: "Shifting Shadows",
     database: "cms"
   });
-
-  async function getManagerNames() {
+  
+/*
+  Start of calls to the database 
+*/
+async function getManagerNames() {
     let query = "SELECT * FROM employee WHERE manager_id IS NULL";
 
     const rows = await db.query(query);
@@ -56,6 +59,7 @@ async function getDepartmentId(departmentName) {
     return rows[0].id;
 }
 
+// Given the name of the role, what is its id?
 async function getRoleId(roleName) {
     let query = "SELECT * FROM role WHERE role.title=?";
     let args = [roleName];
@@ -92,86 +96,6 @@ async function viewAllRoles() {
     const rows = await db.query(query);
     console.table(rows);
     return rows;
-}
-
-async function viewAllDepartments() {
-    // SELECT * from department;
-
-    let query = "SELECT * FROM department";
-    const rows = await db.query(query);
-    console.table(rows);
-}
-
-async function viewAllEmployees() {
-    console.log("");
-
-    // SELECT * FROM employee;
-    let query = "SELECT * FROM employee";
-    const rows = await db.query(query);
-    console.table(rows);
-}
-
-async function viewAllEmployeesByDepartment() {
-    // View all employees by department
-    // SELECT first_name, last_name, department.name FROM ((employee INNER JOIN role ON role_id = role.id) INNER JOIN department ON department_id = department.id);
-    console.log("");
-    let query = "SELECT first_name, last_name, department.name FROM ((employee INNER JOIN role ON role_id = role.id) INNER JOIN department ON department_id = department.id);";
-    const rows = await db.query(query);
-    console.table(rows);
-}
-
-// Will return an array with only two elements in it: 
-// [first_name, last_name]
-function getFirstAndLastName( fullName ) {
-    // If a person has a space in their first name, such as "Mary Kay", 
-    // then first_name needs to ignore that first space. 
-    // Surnames generally do not have spaces in them so count the number
-    // of elements in the array after the split and merge all before the last
-    // element.
-    let employee = fullName.split(" ");
-    if(employee.length == 2) {
-        return employee;
-    }
-
-    const last_name = employee[employee.length-1];
-    let first_name = " ";
-    for(let i=0; i<employee.length-1; i++) {
-        first_name = first_name + employee[i] + " ";
-    }
-    return [first_name.trim(), last_name];
-}
-
-async function updateEmployeeRole(employeeInfo) {
-    // Given the name of the role, what is the role id?
-    // Given the full name of the employee, what is their first_name and last_name?
-    // UPDATE employee SET role_id=1 WHERE employee.first_name='Mary Kay' AND employee.last_name='Ash';
-    const roleId = await getRoleId(employeeInfo.role);
-    const employee = getFirstAndLastName(employeeInfo.employeeName);
-
-    let query = 'UPDATE employee SET role_id=? WHERE employee.first_name=? AND employee.last_name=?';
-    let args=[roleId, employee[0], employee[1]];
-    const rows = await db.query(query, args);
-    console.log(`Updated employee ${employee[0]} ${employee[1]} with role ${employeeInfo.role}`);
-}
-
-async function addEmployee(employeeInfo) {
-    let roleId = await getRoleId(employeeInfo.role);
-    let managerId = await getEmployeeId(employeeInfo.manager);
-
-    // INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Hope", 8, 5);
-    let query = "INSERT into employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)";
-    let args = [employeeInfo.first_name, employeeInfo.last_name, roleId, managerId];
-    const rows = await db.query(query, args);
-    console.log(`Added employee ${employeeInfo.first_name} ${employeeInfo.last_name}.`);
-}
-
-async function removeEmployee(employeeInfo) {
-    const employeeName = getFirstAndLastName(employeeInfo.employeeName);
-    // DELETE from employee WHERE first_name="Cyrus" AND last_name="Smith";
-    let query = "DELETE from employee WHERE first_name=? AND last_name=?";
-    let args = [employeeName[0], employeeName[1]];
-    const rows = await db.query(query, args);
-    console.log(`Employee removed: ${employeeName[0]} ${employeeName[1]}`);
 }
 
 async function viewAllDepartments() {
@@ -299,6 +223,43 @@ async function mainPrompt() {
             }
         ])
 }
+
+async function getAddEmployeeInfo() {
+    const managers = await getManagerNames();
+    const roles = await getRoles();
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?"
+            },
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "role",
+                choices: [
+                    // populate from db
+                    ...roles
+                ]
+            },
+            {
+                type: "list",
+                message: "Who is the employee's manager?",
+                name: "manager",
+                choices: [
+                    // populate from db
+                    ...managers
+                ]
+            }
+        ])
+}
+
 async function getRemoveEmployeeInfo() {
     const employees = await getEmployeeNames();
     return inquirer
@@ -450,42 +411,7 @@ async function main() {
     }
 }
 
-async function getAddEmployeeInfo() {
-    const managers = await getManagerNames();
-    const roles = await getRoles();
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "first_name",
-                message: "What is the employee's first name?"
-            },
-            {
-                type: "input",
-                name: "last_name",
-                message: "What is the employee's last name?"
-            },
-            {
-                type: "list",
-                message: "What is the employee's role?",
-                name: "role",
-                choices: [
-                    // populate from db
-                    ...roles
-                ]
-            },
-            {
-                type: "list",
-                message: "Who is the employee's manager?",
-                name: "manager",
-                choices: [
-                    // populate from db
-                    ...managers
-                ]
-            }
-        ])
-}
-
+// Close your database connection when Node exits
 process.on("exit", async function(code) {
     await db.close();
     return console.log(`About to exit with code ${code}`);
